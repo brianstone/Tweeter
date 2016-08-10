9abec9c9fd86b604b0cc4aad43d8b603ec8e233c
Brian Stone <brianpatrickstone@gmail.com>
2016-08-09 21:12:37 -0600

    Updated database for postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160804150858) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: :cascade do |t|
     t.text     "text"
     t.text     "user"
